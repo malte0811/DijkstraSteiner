123fbc157e4cf4921d21686fe44b94d48a08abe1
malte0811 <malte0811@web.de>
2021-06-14 08:09:13 +0200

    Split BB future cost into cpp file

--- src/future_costs/BBFutureCost.h
+++ src/future_costs/BBFutureCost.h
@@ -6,22 +6,7 @@
 struct BBFutureCost {
     HananGrid const& grid;
 
-    Cost operator()(Label const& label) const {
-        auto grid_min = label.first.indices;
-        auto grid_max = label.first.indices;
-        auto const& terminals = grid.get_terminals();
-        for_each_set_bit(~label.second, terminals.size(), [&](auto const terminal) {
-            grid_min = terminals[terminal].min(grid_min);
-            grid_max = terminals[terminal].max(grid_max);
-        });
-        auto const min_coords = grid.to_coordinates(grid_min);
-        auto const max_coords = grid.to_coordinates(grid_max);
-        Cost result = 0;
-        for (std::size_t dim = 0; dim < num_dimensions; ++dim) {
-            result += max_coords.at(dim) - min_coords.at(dim);
-        }
-        return result;
-    }
+    Cost operator()(Label const& label) const;
 };
 
 #endif
